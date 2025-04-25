85bb4e3017c806f1baea2b7776b8b3278efb0e4d
cristalccm <157647714+cristalccm@users.noreply.github.com>
2025-04-25 21:13:44 +0000

    added accordion to rental page with js and css

--- buffalosedge/assets/js/script.js
+++ buffalosedge/assets/js/script.js
@@ -13,4 +13,18 @@ function rotateCarousel(direction) {
   currAngle += angle * direction;
   carousel.style.transform = `rotateY(${currAngle}deg)`;
 }
+const items = document.querySelectorAll('.accordion button');
 
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
+  }
+
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+items.forEach((item) => item.addEventListener('click', toggleAccordion));
